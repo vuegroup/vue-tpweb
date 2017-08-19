b731712d1d257db5fbc99ca8eee387b208173bcf
tpxqz <xqz101361@163.com>
2017-08-19 14:58:09 +0800

    修改eslint规范

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,8 @@
-<<<<<<< HEAD
+
 import Vue from 'vue'
 import Vuex from 'vuex'
 // import app from './modules/app'
-// import user from './modules/user'
+import user from './modules/user'
 // import permission from './modules/permission'
 import getters from './getters'
 
@@ -11,27 +11,10 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   modules: {
     // app,
-    // user,
+    user
     // permission
   },
   getters
 })
 
 export default store
-=======
-
-import Vue from 'vue';
-import Vuex from 'vuex';
-import user from './modules/user';
-
-Vue.use(Vuex);
-
-const store = new Vuex.Store({
-  modules: {
-    user
-  },
-  getters
-});
-
-export default store
->>>>>>> e45379c4ac259799d7dde428c08ee411de6ded79
