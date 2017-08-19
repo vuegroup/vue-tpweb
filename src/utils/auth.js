0753ed2280f3e61129999d7eb259ec6f0667ec31
tpxqz <xqz101361@163.com>
2017-08-19 11:31:05 +0800

    add httpservice

---
+++ src/utils/auth.js
@@ -0,0 +1,25 @@
+import Cookies from 'js-cookie'
+
+const TokenKey = 'User-Token'
+
+/**
+ * 获取token
+ */
+export function getToken () {
+  return Cookies.get(TokenKey)
+}
+
+/**
+ * 设置token
+ * @param {*值} token
+ */
+export function setToken (token) {
+  return Cookies.set(TokenKey, token)
+}
+
+/**
+ * 移除token
+ */
+export function removeToken () {
+  return Cookies.remove(TokenKey)
+}
